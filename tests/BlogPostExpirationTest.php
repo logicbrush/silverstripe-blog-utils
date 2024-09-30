0d0fcc9c05ca9a597e6e5d0a1caeca359381ecc8
John Learn <jflearn@logicbrush.com>
2024-09-30 09:46:13 -0400

    test fix

--- tests/BlogPostExpirationTest.php
+++ tests/BlogPostExpirationTest.php
@@ -5,6 +5,7 @@ namespace Logicbrush\BlogUtils\Tests;
 use SilverStripe\Dev\FunctionalTest;
 use Logicbrush\BlogUtils\Cron\BlogPostExpirationTask;
 use SilverStripe\Blog\Model\BlogPost;
+use SilverStripe\Blog\Model\Blog;
 
 class BlogPostExpirationTest extends FunctionalTest {
 
@@ -12,13 +13,24 @@ class BlogPostExpirationTest extends FunctionalTest {
 
 	public function setUp() : void {
 		parent::setUp();
+
+		// Create the cron job.
 		$this->cron = new BlogPostExpirationTask();
+
+		// Create the blog container.
+		$this->blog = Blog::create([
+			'Title' => 'Test Blog',
+		]);
+		$this->blog->write();
+		$this->blog->publishSingle();
+
 	}
 
 	public function testArchivingOfExpiredBlogPosts() {
 		
 		$post = BlogPost::create([
 			'Title' => 'Test Post #1',
+			'ParentID' => $this->blog->ID,
 			'ExpirationDate' => '2001-01-01', // expired.
 		]);
 		$post->write();
