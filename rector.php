ff057a6b4035fa7bb1f9cbd61f69e94b5977dea7
John Learn <jflearn@logicbrush.com>
2025-12-10 14:42:35 -0500

    skip override rector

--- rector.php
+++ rector.php
@@ -12,13 +12,14 @@ use Rector\CodeQuality\Rector\If_\ExplicitBoolCompareRector;
 use Rector\Config\RectorConfig;
 use Rector\DeadCode\Rector\Property\RemoveUnusedPrivatePropertyRector;
 use Rector\DeadCode\Rector\StaticCall\RemoveParentCallWithoutParentRector;
+use Rector\Php83\Rector\ClassMethod\AddOverrideAttributeToOverriddenMethodsRector;
 use Rector\Set\ValueObject\LevelSetList;
 use Rector\Set\ValueObject\SetList;
 
 return static function ( RectorConfig $rectorConfig ): void {
 
 	$rectorConfig->sets( [
-			SilverstripeLevelSetList::UP_TO_SILVERSTRIPE_54,
+			SilverstripeLevelSetList::UP_TO_SILVERSTRIPE_60,
 			SilverstripeSetList::CODE_QUALITY,
 			LevelSetList::UP_TO_PHP_83,
 			SetList::CODE_QUALITY,
@@ -26,6 +27,7 @@ return static function ( RectorConfig $rectorConfig ): void {
 		] );
 
 	$rectorConfig->skip( [
+			AddOverrideAttributeToOverriddenMethodsRector::class,	
 			CompleteDynamicPropertiesRector::class,
 			ExplicitBoolCompareRector::class,
 			RemoveParentCallWithoutParentRector::class,
